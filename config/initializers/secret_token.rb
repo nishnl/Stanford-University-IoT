4df762f8bd815b82fb9333f9c341c31fc937da06
Nish <nishnl@hotmail.com>
2013-05-31 21:26:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iotmarketplace::Application.config.secret_token = '6e2a26b0643b699ad7981cdcecd56e05727643b0bb8f2eaa5b87628497be92275845fb5b848ba99c17a29e1f344b12accba4ac562be5fe66b75c9868faad9135'
